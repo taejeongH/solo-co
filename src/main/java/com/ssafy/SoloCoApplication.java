cd83639929b4b67085316741675ebc9b152b5afc
htj7613 <htj7613@naver.com>
2025-11-18 15:09:18 +0900

    init: setting

---
+++ src/main/java/com/ssafy/SoloCoApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloCoApplication.class, args);
+	}
+
+}
