ca804888cbbe097c20188dcbe6e2a5b4130c6070
taejeongH98 <taejeongH98@gmail.com>
2025-11-25 22:38:18 +0900

    feat: 로그아웃 기능 구현

---
+++ src/main/java/com/ssafy/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.interceptor.RequestLogger;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RequestLogger requestLogger;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLogger);
+    }
+}
