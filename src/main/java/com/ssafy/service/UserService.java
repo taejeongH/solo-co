a0d0520e1b93ef0bc40ff0db25810629965168aa
taejeongH98 <taejeongH98@gmail.com>
2025-11-25 22:03:31 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/ssafy/service/UserService.java
+++ src/main/java/com/ssafy/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.ssafy.authentication.JwtTokenProvider;
 import com.ssafy.dto.request.LoginRequestDto;
+import com.ssafy.dto.request.SignupRequestDto;
 import com.ssafy.dto.response.LoginResponseDto;
 import com.ssafy.entity.User;
 import com.ssafy.mapper.UserMapper;
@@ -38,6 +39,25 @@ public class UserService {
                 .email(user.getEmail())
                 .name(user.getName())
                 .build();
+    }
+    
+    public void signup(SignupRequestDto dto) {
+        // username 중복 체크
+        if (userMapper.findByUsername(dto.getUsername()) != null) {
+            throw new IllegalArgumentException("이미 존재하는 사용자명입니다.");
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setPassword(encodedPassword);
+        user.setEmail(dto.getEmail());
+        user.setName(dto.getName());
+        user.setProfileImage(dto.getProfileImage());
+
+        userMapper.insertUser(user);
     }
 }
 
