7ebd1106e90d75b5f03877517f1b90a5d18a50fb
taejeongH98 <taejeongH98@gmail.com>
2025-11-25 21:49:37 +0900

    feat: 로그인 기능 구현

---
+++ src/main/java/com/ssafy/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.ssafy.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.dto.request.LoginRequestDto;
+import com.ssafy.dto.response.LoginResponseDto;
+import com.ssafy.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto) {
+        return ResponseEntity.ok(userService.login(requestDto));
+    }
+}
+
