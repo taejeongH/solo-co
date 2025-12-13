ab534a1f8d85982909314d02dbf7708813d68213
taejeongH <taejeongH98@gmail.com>
2025-12-13 16:14:04 +0900

    fix: swagger 오류 해결

--- src/main/java/com/ssafy/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
